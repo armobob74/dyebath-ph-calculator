d1965bb651e906b98b6d45a9278a01067c193e4a
Armon <armon@nudgex.net>
2023-02-15 19:22:45 -0800

    got calculator basics working, but correctness is way off.

--- calculate_ph.py
+++
@@ -1,5 +0,0 @@
-import math
-kb = {
-        'na2co3':1.659586e-4, # from wikipedia, where they say pka = 10.22
-}
-ph = lambda c: 14 + 0.5 * math.log(kb['na2co3'] * c,10)
--- script.js
+++ script.js
@@ -1,31 +1,24 @@
-    function calculateDebt(rate,time_in_years,total_med_school_cost){
-    let p = total_med_school_cost / 4; // #yearly cost;
-    let r = rate;
-    let t = time_in_years;
-    let a = [p*r**(t), p*r**(t-1), p*r**(t-2), p*r**(t-3)];
-    let message = `
-    Your total tuition will be <strong>$${total_med_school_cost}k</strong> over 4 years.<br>
-    To pay this, you get 1 loan every year for $${p}k.<br>
-    You get a <strong>${(100*(rate-1)).toFixed(2)}%</strong> interest rate and don't pay anything for <strong>${t} years</strong><br>
-    By the time you start making payments, you will owe...<br>
-    `;
-    let sum_a = a[0] + a[1] + a[2] + a[3];
-    let cost = `$${sum_a.toFixed(3)}k`;
-    console.log(message)
-    console.log(cost)
-    return {message: message, cost:cost}
-    }
-    function updatePage(){
+const kb = {
+	'na2co3':1.659586e-4, //from wikipedia, where they say pka = 10.22
+}
+const molar_mass = {
+	'na2co3': 105.99, // g/mol, from wikipedia.
+}
+function calculate_mass(target_ph,water_vol){
+	let mass_required = 10 ** (2 * (target_ph - 14) + Math.log10(kb['na2co3']/(molar_mass['na2co3']* water_vol)))
+	let message = `To get a a <strong>${water_vol}</strong> L solution with a pH of <strong>${target_ph}</strong>, you need <strong>${mass_required}g</strong>  of Soda Ash`;
+	return {message: message, mass_required:mass_required}
+}
+function updatePage(){
 
-    let rate = document.getElementById('rate').value;
-    let t = document.getElementById('t').value;
-    let cost = document.getElementById('cost').value;
+	let target_ph = document.getElementById('target_ph').value;
+	let water_vol = document.getElementById('water_vol').value;
 
-    if(!rate)rate = 1.06;
-    if(!t)t = 8;
-    if(!cost)cost = 244;
-    let data = calculateDebt(rate, t, cost);
-    document.getElementById('display_message').innerHTML = data.message;
-    document.getElementById('display_cost').innerHTML = data.cost;
-    }
-    
+	if(!target_ph)target_ph = 11.00;
+	if(!water_vol)water_vol = 3;
+
+	let data = calculate_mass(target_ph,water_vol);
+	document.getElementById('display_message').innerHTML = data.message;
+	document.getElementById('display_cost').innerHTML = data.mass_required;
+}
+
