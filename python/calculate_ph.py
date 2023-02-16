a3d66179685d3592f3ac5ca1dbae147f00e80b91
Armon <armon@nudgex.net>
2023-02-15 19:22:21 -0800

    moved python files to their own folder

---
+++ python/calculate_ph.py
@@ -0,0 +1,11 @@
+import math
+kb = {
+        'na2co3':1.659586e-4, # from wikipedia, where they say pka = 10.22
+}
+kbn = kb['na2co3']
+ph = lambda c: 14 + 0.5 * math.log(kb['na2co3'] * c,10)
+
+ph_from_mass = lambda m,V: 14 + 0.5 * math.log(kbn * m / (105.99 * V),10)
+
+v0 = 3 #liters
+m = lambda pH: 10 ** (2 * (pH - 14) + math.log(kb['na2co3']/(105.99 * v0),10))
