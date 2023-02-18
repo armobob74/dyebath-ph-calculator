2fbc547ca56035e7bb5ecb3d44c4ed8a0593b71b
Armon <armon@nudgex.net>
2023-02-17 18:58:00 -0800

    fixed algebreic error in m function, and added m1 for more efficient calculation

--- python/calculate_ph.py
+++ python/calculate_ph.py
@@ -8,4 +8,5 @@ ph = lambda c: 14 + 0.5 * math.log(kb['na2co3'] * c,10)
 ph_from_mass = lambda m,V: 14 + 0.5 * math.log(kbn * m / (105.99 * V),10)
 
 v0 = 3 #liters
-m = lambda pH: 10 ** (2 * (pH - 14) + math.log(kb['na2co3']/(105.99 * v0),10))
+m = lambda pH: 10 ** (2 * (pH - 14) - math.log(kb['na2co3']/(105.99 * v0),10))
+m1 = lambda pH: 10 ** (2 * (pH - 14)) * 105.99 * v0 / kbn
